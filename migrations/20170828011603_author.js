cbb771c9b2585eeb40e93e762af5ad70f041e472
Levi Holmes <levi.holmes81@gmail.com>
2017-08-28 02:12:43 -0600

    migration file attributes entered

--- migrations/20170828011603_author.js
+++ migrations/20170828011603_author.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('author' , (table)=>{
+    table.increments()
+    table.text('firstName')
+    table.text('lastName')
+    table.text('portriat')
+    table.text('bio')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTableIfExists('author')
 };
